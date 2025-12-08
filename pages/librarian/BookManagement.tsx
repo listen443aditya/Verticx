6f1f076466fad1dc9b8356a2c280dffc1188cf2e
Aditya Kumar Singh <adityakumarsingh2121@outlook.com>
2025-12-08 12:31:48 +0530

    add barcode scanner feature on librarian portal

--- pages/librarian/BookManagement.tsx
+++ pages/librarian/BookManagement.tsx
@@ -291,7 +291,7 @@ const BookManagement: React.FC = () => {
                       {book.title}
                     </td>
                     <td className="p-4 text-slate-600">{book.author}</td>
-                    <td className="p-4 font-medium text-xs text-slate-500 px-2 py-1 rounded w-fit">
+                    <td className="p-4 font-medium text-xs text-slate-900 px-2 py-1 rounded w-fit">
                       {book.isbn}
                     </td>
                     <td className="p-4 text-center">
